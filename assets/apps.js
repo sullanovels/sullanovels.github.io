7c8857fe13574139d20e4d82f16b77d5b23a832f
sullanovels <detetivekaiju@gmail.com>
2026-06-21 13:26:30 -0300

    Update apps.js

--- assets/apps.js
+++ assets/apps.js
@@ -1,1 +1,23 @@
+async function loadNovel(path) {
+  const info = await fetch(path + "/info.json").then(r => r.json());
 
+  document.getElementById("title").innerText = info.title;
+
+  const list = document.getElementById("chapters");
+  list.innerHTML = "";
+
+  info.chapters.forEach((chapterFile) => {
+    const li = document.createElement("li");
+
+    li.innerText = chapterFile.replace(".md", "");
+
+    li.onclick = async () => {
+      const text = await fetch(path + "/" + chapterFile).then(r => r.text());
+      document.getElementById("content").innerText = text;
+    };
+
+    list.appendChild(li);
+  });
+}
+
+loadNovel("./novels/minha-novel");
